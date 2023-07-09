144cd59c55fdd01043a562710a7de7e5c2d7aa17
jaz801 <34124216+jaz801@users.noreply.github.com>
2023-07-09 10:40:38 +0000

    add csv upload, html and bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Jovian!</p>"
+    return render_template('home.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        filename = file.filename
+        file.save(filename)
+        # Process the CSV file
+        with open(filename, 'r') as csv_file:
+            csv_data = csv.reader(csv_file)
+            for row in csv_data:
+                # Process each row of the CSV file
+                print(row)
+        return 'CSV file uploaded and processed successfully.'
+    else:
+        return 'No file uploaded.'
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
